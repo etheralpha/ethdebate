2f91a32d8a711b262ec0b74a3dbb2eba6951060c
hanniabu <hanniabu@gmail.com>
2026-05-22 18:14:37 -0400

    fix social preview

--- build.py
+++ build.py
@@ -333,6 +333,9 @@ def page_html(
   <meta property="og:description" content="{attr(description)}" />
   <meta property="og:type" content="website" />
   <meta property="og:image" content="{attr(image)}" />
+  <meta property="og:image:type" content="image/png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:image" content="{attr(image)}" />
   <meta name="theme-color" content="{attr(theme_color)}" />
